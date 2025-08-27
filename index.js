d59c8c4f6a6755d56fd5a1fc3ef13611408bc1e4
Mehad Hossen <mehad004938@gmail.com>
2025-08-27 23:18:03 +0600

    add my insuffient coin part

--- index.js
+++ index.js
@@ -21,14 +21,48 @@ const allButton = document.getElementsByClassName("all-button");
          const title=eachButton.parentNode.parentNode.children[1].innerText;
         
           const allNumber = eachButton.parentNode.parentNode.children[2].innerText;
-          
-          alert('📞'+"Calling"+title+allNumber+'...');
-          
-          const beforeCoin = parseInt(
-            document.getElementById("before-coin").innerText
-          );
+          const mainTitle = eachButton.parentNode.parentNode.children[0].innerText;
+         
+           const beforeCoin = parseInt(
+             document.getElementById("before-coin").innerText
+           );
            
+            const currentCoin = beforeCoin - 20;
+
+            if (currentCoin < 0) {
+              alert(
+                "You hove no sufficient coin.You need to 20 coin for  each call"
+              );
+              return;
+            }
+        
+           alert(
+             "📞" + " " + "Calling" + " " + title + " " + allNumber + "..."
+           );
+
+
+        
+          document.getElementById("before-coin").innerText=currentCoin;
+         
           
+          
+           
+        //   calling story start form here 
+         const time = new Date().toLocaleTimeString();
+        const storyContainer = document.getElementById("story-add");
+        const div=document.createElement('div');
+        div.innerHTML = `
+         <div class="flex justify-between py-3 px-3 items-center">
+                   <div>
+                     <h1 class="font-bold">${mainTitle}</h1>
+                    <p>${allNumber}</p>
+                   </div>
+                   <p>${time}</p>
+
+                </div>
+
+        `;
+        storyContainer.append(div);
           
          
         
